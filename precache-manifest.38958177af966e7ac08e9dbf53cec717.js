9cc09749722d84396ec45746583ef3120ae82ab9
kyngstudio <kyngstudio@gmail.com>
2018-10-16 01:40:53 +0200

    First commit

---
+++ precache-manifest.38958177af966e7ac08e9dbf53cec717.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36722e909c85f05ce638",
+    "url": "/static/js/main.36722e90.chunk.js"
+  },
+  {
+    "revision": "07f3c0e381eac854669c",
+    "url": "/static/js/1.07f3c0e3.chunk.js"
+  },
+  {
+    "revision": "36722e909c85f05ce638",
+    "url": "/static/css/main.dc2ea210.chunk.css"
+  },
+  {
+    "revision": "c21dfb4128ebf8a79b097ef601a335cf",
+    "url": "/index.html"
+  }
+];
